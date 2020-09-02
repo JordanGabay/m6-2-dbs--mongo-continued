8134717e7051121953060121b07c59ff7501291e
Jordan Gabay <gabay.jordan@outlook.com>
2020-09-02 13:38:27 -0400

    Exercise 3 and 4 Complete

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,5 @@
-'use strict';
-const {MongoClient} = require("mongodb");
+"use strict";
+const { MongoClient } = require("mongodb");
 
 require("dotenv").config();
 const { MONGO_URI } = process.env;
@@ -9,19 +9,44 @@ const options = {
 };
 
 const getSeats = async (req, res) => {
-    const client = await MongoClient(MONGO_URI, options);
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("exercise_1");
+
+  const data = await db.collection("seats").find().toArray();
+  let seats = {};
+  data.forEach((seat) => {
+    seats[seat._id] = seat;
+  });
+  console.log(seats);
+  client.close();
+  res.status(200).json({ seats: seats, numOfRows: 8, seatsPerRow: 12 });
+  return seats;
+};
+
+const seatPurchase = async (req, res) => {
+  const _id = req.body.seatId;
+  const client = await MongoClient(MONGO_URI, options);
+  try {
     await client.connect();
     const db = client.db("exercise_1");
+    const newValues = {
+      $set: { isBooked: true, user: req.body.fullName, email: req.body.email },
+    };
+    if (req.body === undefined) {
+      throw new Error("Need more Info");
+    }
+    console.log("SEAT_ID", _id);
+    const greetings = await db
+      .collection("seats")
+      .updateOne({ _id }, newValues);
+    res.status(200).json({ status: 200, data: { _id } });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ status: 500, data: { ...req.body }, message: err.message });
+  }
+  client.close();
+};
 
-    const data = await db.collection("seats").find().toArray();
-    let seats = {};
-    data.forEach((seat) => {
-        seats[seat._id] = seat;
-    })
-    console.log(seats);
-    client.close();
-    res.status(200).json({seats: seats, numOfRows: 8, seatsPerRow: 12})
-    return seats;
-}
-
-module.exports = { getSeats };
+module.exports = { getSeats, seatPurchase };
